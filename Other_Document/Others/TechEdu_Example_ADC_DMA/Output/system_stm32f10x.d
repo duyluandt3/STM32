29fa1b57f825089b3faff9c41bc227e900b228d8
duyluandt3 <duyluandt3@gmail.com>
2020-12-21 16:24:56 +0900

    Add other document

---
+++ Other_Document/Others/TechEdu_Example_ADC_DMA/Output/system_stm32f10x.d
@@ -0,0 +1,16 @@
+.\output\system_stm32f10x.o: RTE\Device\STM32F103VE\system_stm32f10x.c
+.\output\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\1.0.5\Device\Include\stm32f10x.h
+.\output\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\3.20.4\CMSIS\Include\core_cm3.h
+.\output\system_stm32f10x.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
+.\output\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\3.20.4\CMSIS\Include\core_cmInstr.h
+.\output\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\3.20.4\CMSIS\Include\core_cmFunc.h
+.\output\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\1.0.5\Device\Include\system_stm32f10x.h
+.\output\system_stm32f10x.o: .\RTE\Device\STM32F103VE\stm32f10x_conf.h
+.\output\system_stm32f10x.o: .\RTE\RTE_Components.h
+.\output\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\1.0.5\Device\StdPeriph_Driver\inc\stm32f10x_adc.h
+.\output\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\1.0.5\Device\Include\stm32f10x.h
+.\output\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\1.0.5\Device\StdPeriph_Driver\inc\stm32f10x_dma.h
+.\output\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\1.0.5\Device\StdPeriph_Driver\inc\stm32f10x_gpio.h
+.\output\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\1.0.5\Device\StdPeriph_Driver\inc\stm32f10x_rcc.h
+.\output\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\1.0.5\Device\StdPeriph_Driver\inc\stm32f10x_usart.h
+.\output\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\1.0.5\Device\StdPeriph_Driver\inc\misc.h
---
+++ Other_Document/STM32F4/F4/15.TFT/TFT/MDK-ARM/TFT/startup_stm32f417xx.d
@@ -0,0 +1,1 @@
+tft\startup_stm32f417xx.o: startup_stm32f417xx.s
